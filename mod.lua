6d3afb27c6ee27e8052d9e7547e6fec08be9bdcd
Florian Thiel <flth@flth.de>
2024-11-29 17:54:35 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,20 @@
+function data()
+return {
+info = {
+  minorVersion = 0,
+  severityAdd = "NONE",
+  severityRemove = "WARNING",
+  name = _("mod_name"),
+  description = _("mod_desc"),
+  authors = {
+        {
+            name = "Bandion",
+            role = "CREATOR",
+        },
+  },
+  tags = {"europe", "assets", "planes" },
+  visible = true,
+},
+
+ }
+end
---
+++ strings.lua
@@ -0,0 +1,16 @@
+function data()
+	return {
+		de = {
+
+			["mod_name"] = "Flugzeug Asset Set 3",
+			["mod_desc"] = "Flugzeugset zum Gestalten von Flugplatzszenen.",
+
+		},
+		en = {
+
+			["mod_name"] = 	"Aircraft Asset Set 3",
+			["mod_desc"] = 	"Aircraft collection to set up airfield scenes.",
+
+		},
+	}
+end
